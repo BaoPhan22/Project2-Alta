2137a915e34d9572de75b509322b94c8d0739331
BaoPhan22 <93857238+BaoPhan22@users.noreply.github.com>
2023-05-31 11:20:59 +0700

    31/5

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,55 +21,55 @@
 </head>
 
 <body>
-    <div class="row">
-        <div class="col-left">
-            <div id="logo-container" class="d-flex justify-content-center align-items-center">
-                <img src="{{ asset('image_layout/Logo alta.svg') }}" alt="Alta Logo">
-            </div>
-            <form method="POST" action="{{ route('login') }}">
-                @csrf
+    <div class="container">
+        <div class="row">
+            <div class="col-left">
+                <div id="logo-container" class="d-flex justify-content-center align-items-center">
+                    <img src="{{ asset('image_layout/Logo alta.svg') }}" alt="Alta Logo">
+                </div>
+                <form method="POST" action="{{ route('login') }}"></form>
+                    @csrf
 
-                <div class="input-fields m-auto">
+                    <div class="input-fields m-auto">
+
+                        <!-- Username -->
+                        <div class="mb-3">
+                            <label for="username" class="form-label">Tên đăng nhập *</label>
+                            <input type="text" class="form-control inputs" id="username" name="username" required>
+                        </div>
 
 
-                    <!-- Username -->
-                    <div class="mb-3">
-                        <label for="username" class="form-label">Tên đăng nhập *</label>
-                        <input type="text" class="form-control inputs" id="username" name="username">
+                        <!-- Password -->
+                        <div class="mb-3 m-auto p-relative">
+                            <label for="password" class="form-label ">Mật khẩu *</label>
+                            <input type="password" class="form-control inputs" id="password" name="password" required>
+                            <span class="p-absolute visible-password bi bi-eye-slash" id="togglePassword"></span>
 
+                        </div>
+
+                        <a href="{{ route('password.request') }}" class="text-danger text-decoration-none">Quên mật khẩu?</a> <br>
                     </div>
 
-
-                    <!-- Password -->
-                    <div class="mb-3 m-auto p-relative">
-                        <label for="password" class="form-label ">Mật khẩu *</label>
-                        <input type="password" class="form-control inputs border border-danger border-2" id="password" name="password">
-                        <span class="p-absolute visible-password bi bi-eye-slash" id="togglePassword"></span>
-
+                    <div id="button-pri" class="mt-3 m-auto">
+                        <button type="submit" id="button-pri">Đăng nhập</button>
                     </div>
-
-                    <a href="{{ route('password.request') }}" class="text-danger text-decoration-none">Quên mật khẩu?</a> <br>
-                </div>
-
-                <div id="button-pri" class="mt-3 m-auto">
-                    <button type="submit" id="button-pri">Đăng nhập</button>
-                </div>
-            </form>
-        </div>
-        <div class="col-right p-relative">
-            <div class="image-svg">
-                <img src="{{ asset('image_layout/Group 341.svg') }}" alt="">
+                </form>
             </div>
-            <p id="system" class="p-absolute">Hệ thống</p>
-            <p id="manage-queue" class="p-absolute">Quản lý xếp hàng</p>
+            <div class="col-right p-relative">
+                <div class="image-svg">
+                    <img src="{{ asset('image_layout/Group 341.svg') }}" alt="">
+                </div>
+                <p id="system" class="p-absolute">Hệ thống</p>
+                <p id="manage-queue" class="p-absolute">Quản lý xếp hàng</p>
+            </div>
         </div>
     </div>
-    
+
     <script>
         const togglePassword = document.querySelector('#togglePassword');
         const password = document.querySelector('#password');
 
-        
+
         togglePassword.addEventListener("click", function() {
 
             const eyeClass = document.querySelector('.bi').classList;
